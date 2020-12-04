2055fb879159c863aa4e3490a32d2efd3b007bd5
fortym2 <30431538+fortym2@users.noreply.github.com>
2020-12-04 21:20:17 +0100

    Initial commit

---
+++ server.py
@@ -0,0 +1,64 @@
+#!/usr/bin/env python
+
+# This script connects to the serial port (the Arduino) to retrieve data
+# from the sensors, which is in form of a JSON string
+# { temperature: <val>, humidity: <val> }.
+#
+# This script then starts a simple HTTP server with two routes:
+# - /api: serves the raw JSON string;
+# - /: serves the file index.html, which displays the data in a prettier way.
+#
+# The library pySerial is required.
+#
+# Author: Davide Quaranta
+# quaranta.1715742@studenti.uniroma1.it
+
+# import needed libraries
+import serial
+from http.server import BaseHTTPRequestHandler, HTTPServer
+
+SERIAL_PORT = 'COM3'    # port on which the Arduino is connected
+HTTP_PORT = 8000        # port for the HTTP server to listen to
+
+# estabilish a serial connection to the Arduino
+print("Attempting to connect to serial port " + SERIAL_PORT + "...")
+ser = serial.Serial(SERIAL_PORT, 9600, timeout=200)
+print("Connected!")
+data = ''
+
+# specify the behavior of our HTTP server,
+# in particular what to do on GET requests.
+class MyHandler(BaseHTTPRequestHandler):  
+    def do_GET(self): 
+        if self.path == "/api":
+            # this is an API call: read from serial port
+            data = ser.readline()
+            print(data)
+            self.send_response(200)                                 # 200: OK
+            self.send_header("Content-type", "application/json")    # this content is JSON
+            self.end_headers()
+            self.wfile.write(data)                                  # write on the response's body
+        elif self.path == "/":
+            # this is a dashboard request: serve the HTML file
+            self.send_response(200)                                 # 200: OK
+            self.send_header("Content-type", "text/html")           # this content is HTML
+            self.end_headers()
+            content = open("index.html", "rb").read()               # read the file as bytes
+            self.wfile.write(content)                               # and put them on the response's body
+
+# start the HTTP daemon on localhost (127.0.0.1) on the specified port
+# with the handler that we have specified
+print("Starting HTTP server...")
+httpd = HTTPServer(("", HTTP_PORT), MyHandler)
+print("HTTP server listening on localhost:" + str(HTTP_PORT))
+try:
+    httpd.serve_forever()
+except KeyboardInterrupt:
+    # don't scream errors if we close the server via keyboard (CTRL+C)
+    pass
+
+# if we get here, it means that we interrupted the HTTP server
+# then close it and the serial communication
+print("Closing HTTP server and serial connection...")
+httpd.server_close()
+ser.close()
---
+++ webserver.ino
@@ -0,0 +1,69 @@
+/**
+ * This Arduino code reads temperature and relative humidity
+ * from a DHT11 sensor, and prints the data to an LCD display
+ * and the serial port, to make the data avilable to other applications.
+ * //TODO: dressing + status leds
+ * Author: Davide Quaranta
+ * quaranta.1715742@studenti.uniroma1.it
+ */
+
+// import required libraries
+#include <stdio.h>
+#include <LiquidCrystal.h>
+#include "DHT.h"
+
+#define DHTPIN 8                          // set the data pin used for the DHT sensr
+#define DHTTYPE DHT11                     // set the type of DHT sensor
+
+// set the backlight pins
+#define CONTRAST_PIN 9
+#define BRIGHTNESS_PIN 10
+
+// initialize the libraries
+LiquidCrystal lcd(12, 11, 5, 4, 3, 2);
+DHT dht(DHTPIN, DHTTYPE);
+
+void setup() {
+  // begin using the LCD
+  lcd.begin(16, 2);
+
+  // mark the backlight pins as output
+  pinMode(CONTRAST_PIN, OUTPUT);
+  pinMode(BRIGHTNESS_PIN, OUTPUT);
+
+  // set the backlight in such a way that characters are visible
+  digitalWrite(CONTRAST_PIN, LOW);
+  digitalWrite(BRIGHTNESS_PIN, HIGH);
+
+  // begin using the DHT 
+  dht.begin();
+  lcd.print("Temp:  Humidity:");
+
+  // begin using the serial port with baudrate 9600
+  Serial.begin(9600);
+  while (!Serial) {
+    ; // wait until the connection is estabilished
+  }
+}
+
+void loop() {
+  delay(2000);                            // add a delay needed to avoid DHT reading errors
+  lcd.setCursor(0, 1);                    // set the cursor on row 1 and column 0
+
+  // read data from the sensor
+  int hum = dht.readHumidity();
+  float temp = dht.readTemperature();
+
+  lcd.print(temp);
+  lcd.setCursor(7,1);                     // set the cursor on row 1 and column 7
+  lcd.print(hum);
+
+  char json[50];                          // prepare a buffer for the JSON stirng
+  
+  // create the JSON string.
+  // The *100%100 trick is used to get the decimals, because Arduino doesn't support %f
+  snprintf(json, 50, "{\"temperature\":%d.%02d,\"humidity\":%d}\n", (int)temp, (int)(temp*100)%100, hum);
+
+  Serial.write(json);                     // write the JSON string on the serial port
+  Serial.flush();                         // wait for the transmission to finish
+}
